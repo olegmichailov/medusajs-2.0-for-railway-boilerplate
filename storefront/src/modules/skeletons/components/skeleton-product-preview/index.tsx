1e344ab0ab42911c2a67ac61ea49f8a61816fbd6
olegmichailov <olegmikhailov698@gmail.com>
2025-04-21 14:44:20 +0200

    Update index.tsx

--- storefront/src/modules/skeletons/components/skeleton-product-preview/index.tsx
+++ storefront/src/modules/skeletons/components/skeleton-product-preview/index.tsx
@@ -1,10 +1,12 @@
+import { Container } from "@medusajs/ui"
+
 const SkeletonProductPreview = () => {
   return (
     <div className="animate-pulse">
-      <div className="aspect-[1/1] w-full bg-gray-100" />
+      <Container className="aspect-[3/4] w-full bg-gray-100" />
       <div className="flex justify-between text-base-regular mt-2">
-        <div className="w-2/5 h-6 bg-gray-100" />
-        <div className="w-1/5 h-6 bg-gray-100" />
+        <div className="w-2/5 h-6 bg-gray-100"></div>
+        <div className="w-1/5 h-6 bg-gray-100"></div>
       </div>
     </div>
   )
