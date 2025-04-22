1041fa4b0a69b095c1cc6fca87f8c2cc917940a6
olegmichailov <olegmikhailov698@gmail.com>
2025-04-22 02:07:23 +0200

    Update page.tsx

--- storefront/src/app/[countryCode]/darkroom/page.tsx
+++ storefront/src/app/[countryCode]/darkroom/page.tsx
@@ -1,14 +1,6 @@
-"use client"
-
-import EditorCanvas from "@/modules/darkroom/EditorCanvas"
+"use client";
+import EditorCanvas from "@/modules/darkroom/EditorCanvas";
 
 export default function DarkroomPage() {
-  return (
-    <div className="px-6 pt-10">
-      <h1 className="text-4xl tracking-wider uppercase mb-8 font-medium">
-        Darkroom Editor
-      </h1>
-      <EditorCanvas />
-    </div>
-  )
+  return <EditorCanvas />;
 }
