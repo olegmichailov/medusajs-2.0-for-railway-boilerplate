e53d6a02cd1da092c99300a537166fef3519a66d
olegmichailov <olegmikhailov698@gmail.com>
2025-04-22 23:09:01 +0200

    Update paginated-products.tsx

--- storefront/src/modules/store/templates/paginated-products.tsx
+++ storefront/src/modules/store/templates/paginated-products.tsx
@@ -9,7 +9,7 @@ import { SortOptions } from "@modules/store/components/refinement-list/sort-prod
 
 const PRODUCT_LIMIT = 12
 
-const columnOptionsMobile = [1, 2]
+const columnOptionsMobile = [1]
 const columnOptionsDesktop = [1, 2, 3, 4]
 
 type PaginatedProductsParams = {
@@ -90,7 +90,7 @@ export default function PaginatedProducts({
 
   return (
     <>
-      <div className="px-6 pt-4 pb-2 flex items-center justify-between sm:justify-between">
+      <div className="px-4 sm:px-6 pt-4 pb-2 flex items-center justify-between">
         <div className="text-sm sm:text-base font-medium tracking-wide uppercase">
           {/* Динамический заголовок будет здесь от другого компонента */}
         </div>
@@ -112,7 +112,7 @@ export default function PaginatedProducts({
       </div>
 
       <ul
-        className={`grid ${gridColsClass} gap-x-4 gap-y-10 px-6 sm:px-0`}
+        className={`grid ${gridColsClass} gap-x-4 gap-y-10 px-4 sm:px-0`}
         data-testid="products-list"
       >
         {products.map((p) => (
